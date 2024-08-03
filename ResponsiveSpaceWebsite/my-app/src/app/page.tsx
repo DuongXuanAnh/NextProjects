c8fb99aed189202801f7b0fc7ea0cd44f1e225ea
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2024-08-03 20:13:24 +0200

    Hero section with tailwind

--- ResponsiveSpaceWebsite/my-app/src/app/page.tsx
+++ ResponsiveSpaceWebsite/my-app/src/app/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "@/components/Navbar/Navbar";
+import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
   return (
@@ -12,8 +13,9 @@ export default function Home() {
           muted
           className="fixed right-0 top-0 h-[700px] w-full object-cover z-[-1]"
         />
+        <Navbar />
+        <Hero />
       </div>
-      <Navbar />
     </div>
   );
 }
