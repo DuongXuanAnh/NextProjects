590b52e49ec4bb695d327bf9de17ff0ba1646df3
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2024-08-06 11:49:29 +0200

    frame-motion menu

--- tailwind_practice/src/app/page.tsx
+++ tailwind_practice/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Search from "@/components/Search/Search";
+import FrameMotionMenu from "@/components/FrameMotion/FrameMotionMenu";
 
 export default function Home() {
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-[#E6EFFA]">
-      <Search />
+    <div className="flex h-screen w-full items-center justify-center bg-[#E6EFFA] text-black">
+      <FrameMotionMenu />
     </div>
   );
 }
