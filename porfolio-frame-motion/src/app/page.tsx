4e6d21b6dce8acf280e25d22d75a03a33c08a82d
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2024-08-07 16:10:01 +0200

    Init project && Scroll snap

---
+++ porfolio-frame-motion/src/app/page.tsx
@@ -0,0 +1,19 @@
+import Navbar from "@/components/navbar/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <section className="h-screen snap-center">
+        <Navbar />
+      </section>
+      <section className="h-screen snap-center">Section 2</section>
+      <section className="h-screen snap-center">Section 3</section>
+      <section className="h-screen snap-center">Section 4</section>
+      <section className="h-screen snap-center">Section 5</section>
+      <section className="h-screen snap-center">Section 6</section>
+      <section className="h-screen snap-center">Section 7</section>
+      <section className="h-screen snap-center">Section 8</section>
+    </div>
+  );
+}
