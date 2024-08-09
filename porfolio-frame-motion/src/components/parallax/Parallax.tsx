9d56712247f78370f33e8ccc0dbff28553b28d13
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2024-08-09 13:38:53 +0200

    Animation(Parallax) - Scroll effect

--- porfolio-frame-motion/src/components/parallax/Parallax.tsx
+++ porfolio-frame-motion/src/components/parallax/Parallax.tsx
@@ -1,9 +1,11 @@
 "use client";
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
 import mountains from "../../assets/mountains.png";
 import planets from "../../assets/planets.png";
 import stars from "../../assets/stars.png";
+import sun from "../../assets/sun.png";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const Parallax = ({ type }: { type: string }) => {
   const background =
@@ -11,22 +13,43 @@ const Parallax = ({ type }: { type: string }) => {
       ? "bg-gradient-to-b from-[#0c0c1d] to-[#111132]"
       : "bg-gradient-to-b from-[#0c0c1d] to-[#505064]";
 
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
   return (
     <div
-      className={`${background} relative flex h-full w-full items-center justify-center`}
+      className={`${background} relative flex h-full w-full items-center justify-center overflow-hidden`}
+      ref={ref}
     >
-      <h1 className="text-[100px] font-bold">
+      <motion.h1 className="text-[70px] font-bold" style={{ y: yText }}>
         {type === "services" ? "What We Do?" : "What We Did?"}
-      </h1>
-      <div className="absolute h-full w-full bg-cover bg-bottom">
+      </motion.h1>
+      <motion.div className="absolute z-[3] h-full w-full bg-cover bg-bottom">
         <Image src={mountains} alt="Mountains" layout="fill" />
-      </div>
-      <div className="absolute h-full w-full bg-cover bg-bottom">
-        <Image src={planets} alt="planets" layout="fill" />
-      </div>
-      <div className="absolute h-full w-full bg-cover bg-bottom">
+      </motion.div>
+      <motion.div
+        className="absolute z-[2] h-full w-full bg-cover bg-bottom"
+        style={{ y: yBg }}
+      >
+        {type === "services" ? (
+          <Image src={planets} alt="planets" layout="fill" />
+        ) : (
+          <Image src={sun} alt="stars" layout="fill" />
+        )}
+      </motion.div>
+      <motion.div
+        className="absolute z-[1] h-full w-full bg-cover bg-bottom"
+        style={{ x: yBg }}
+      >
         <Image src={stars} alt="stars" layout="fill" />
-      </div>
+      </motion.div>
     </div>
   );
 };
