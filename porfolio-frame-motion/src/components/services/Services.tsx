ea0e4a93cbe9cf34f82e7a15c3b05a8ac33f68bb
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2024-08-09 20:46:40 +0200

    Animation(Services) - UseInView

--- porfolio-frame-motion/src/components/services/Services.tsx
+++ porfolio-frame-motion/src/components/services/Services.tsx
@@ -1,14 +1,34 @@
 "use client";
-import React from "react";
-import { motion } from "framer-motion";
+import React, { use, useRef } from "react";
+import { motion, useInView } from "framer-motion";
 
 import Text from "./containers/Text";
 import Title from "./containers/Title";
 import List from "./containers/List";
 
+const variants = {
+  initial: { x: -500, y: 100, opacity: 0 },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: { duration: 1, staggerChildren: 0.1 },
+  },
+};
+
 const Services = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { margin: "-100px" });
+
   return (
-    <motion.div className="flex h-full flex-col justify-between bg-gradient-to-b from-[#0c0c1d] to-[#111132]">
+    <motion.div
+      className="flex h-full flex-col justify-between bg-gradient-to-b from-[#0c0c1d] to-[#111132]"
+      variants={variants}
+      initial="initial"
+      // whileInView="animate"
+      ref={ref}
+      animate={isInView ? "animate" : "initial"}
+    >
       <Text />
       <Title />
       <List />
--- porfolio-frame-motion/src/components/services/containers/Title.tsx
+++ porfolio-frame-motion/src/components/services/containers/Title.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/image";
 import peopleWebp from "../../../assets/people.webp";
+import { motion } from "framer-motion";
 
 const Title = () => {
   return (
-    <div className="flex flex-col items-center">
+    <motion.div className="flex flex-col items-center">
       <div className="flex items-center gap-12">
         <div>
           <Image
@@ -14,19 +15,20 @@ const Title = () => {
           />
         </div>
         <h1 className="text-[80px]">
-          <b>Unique</b> Ideas
+          <motion.b whileHover={{ color: "#f97316" }}>Unique</motion.b> Ideas
         </h1>
       </div>
 
       <div className="flex items-center gap-10">
         <h1 className="text-[80px]">
-          <b>For Your</b> Business.
+          <motion.b whileHover={{ color: "#f97316" }}>For Your</motion.b>{" "}
+          Business.
         </h1>
         <button className="h-[80px] w-[250px] cursor-pointer rounded-[50px] bg-orange-500 text-xl">
           WHAT WE DO?
         </button>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
