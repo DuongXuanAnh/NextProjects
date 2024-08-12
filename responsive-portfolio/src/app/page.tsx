9f0506d166cbb99923ba456480052989c50af361
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2024-08-12 20:35:27 +0200

    Work experience

--- responsive-portfolio/src/app/page.tsx
+++ responsive-portfolio/src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/hero/Hero";
 import Navbar from "@/components/navbar/Navbar";
 import Projects from "@/components/projects/Projects";
 import Skills from "@/components/skills/Skills";
+import WorkExperience from "@/components/workExperience/WorkExperience";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <Projects />
         <Bio />
         <Skills />
+        <WorkExperience />
       </div>
     </div>
   );
